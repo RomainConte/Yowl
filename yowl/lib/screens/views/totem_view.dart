fbe53ca1ba08d652b5ed46c1df1b07151be6a6fa
guillaumebozec <guillaume.bozec@epitech.digital>
2024-01-29 18:35:27 +0100

    [PUT] Start to build totem page

--- yowl/lib/screens/views/totem_view.dart
+++ yowl/lib/screens/views/totem_view.dart
@@ -1,11 +1,60 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class TotemView extends StatelessWidget {
-  const TotemView({super.key});
+  const TotemView({Key? key});
+
+  Future<List<dynamic>> fetchAnimals() async {
+    final response = await http.get(Uri.parse('http://10.0.2.2:1337/api/animals/1'));
+
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+
+      if (responseData.containsKey('data')) {
+        return List<dynamic>.from(responseData['data']);
+      } else {
+        throw Exception('Expected key "data" not found in response');
+      }
+    } else {
+      throw Exception('Failed to load animals');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Totem'),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animals'),
+      ),
+      body: FutureBuilder<List<dynamic>>(
+        future: fetchAnimals(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No data available'));
+          } else if (snapshot.hasData) {
+            final animalsList = snapshot.data as List<dynamic>;
+
+            return ListView.builder(
+              itemCount: animalsList.length,
+              itemBuilder: (context, index) {
+                final animals = animalsList[index]['attributes'];
+
+                return ListTile(
+                  title: Text('Name: ${animals['name']}'),
+                  subtitle: Text('Vmax: ${animals['vmax']}'),
+                );
+              },
+            );
+          } else {
+            return Center(child: Text('Something went wrong'));
+          }
+        },
+      ),
+    );
   }
 }
