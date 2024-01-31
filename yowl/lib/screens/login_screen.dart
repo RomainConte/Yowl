4c3891ad3352c547ace980d9fbe7e8cc704a74ba
Anafaure <anaelle.faure@epitech.digital>
2024-01-31 14:38:45 +0100

    [PUT] Final front-login with button center and scroll

--- yowl/lib/screens/login_screen.dart
+++ yowl/lib/screens/login_screen.dart
@@ -58,106 +58,112 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const MyAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(50),
-                      boxShadow: [
-                        BoxShadow(
-                          color: const Color.fromARGB(255, 202, 202, 202),
-                          blurRadius: 10,
-                          offset: Offset(0, 2.5),
+      body: SingleChildScrollView(
+        
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(50),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(255, 202, 202, 202),
+                            blurRadius: 10,
+                            offset: Offset(0, 2.5),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                          labelText: "Email",
+                          contentPadding: EdgeInsets.all(10),
+                          border: InputBorder.none,
                         ),
-                      ],
+                      ),
                     ),
-                    child: TextField(
-                      controller: emailController,
-                      decoration: const InputDecoration(
-                        labelText: "Email",
-                        contentPadding: EdgeInsets.all(10),
-                        border: InputBorder.none,
+                  )
+                ],
+              ),
+              SizedBox(height: 50),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(50),
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(255, 202, 202, 202),
+                            blurRadius: 10,
+                            offset: Offset(0, 2.5),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: 'Mot de passe',
+                          contentPadding: EdgeInsets.all(10),
+                          border: InputBorder.none,
+                        ),
                       ),
                     ),
                   ),
-                )
-              ],
-            ),
-            SizedBox(height: 50),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(50),
-                      boxShadow: [
-                        BoxShadow(
-                          color: const Color.fromARGB(255, 202, 202, 202),
-                          blurRadius: 10,
-                          offset: Offset(0, 2.5),
-                        ),
-                      ],
-                    ),
-                    child: TextField(
-                      controller: passwordController,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        labelText: 'Mot de passe',
-                        contentPadding: EdgeInsets.all(10),
-                        border: InputBorder.none,
-                      ),
+                ],
+              ),
+              SizedBox(height: 30),
+              // Column(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   crossAxisAlignment: CrossAxisAlignment.center,
+              //   children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Color.fromRGBO(241, 185, 28, 1),
+                          onPrimary: Colors.white,
+                          elevation: 3,
+                          maximumSize: const Size.fromWidth(200)),
+                      onPressed: login,
+                      child: Text('Se connecter'),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Row(
-                  children: [
-                    SizedBox(
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Color.fromRGBO(241, 185, 28, 1),
-                            onPrimary: Colors.white,
-                            elevation: 3,
-                            maximumSize: const Size.fromWidth(200)),
-                        onPressed: login,
-                        child: Text('Se connecter'),
-                      ),
+                ],
+              ),
+              SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          onPrimary: Colors.black,
+                          elevation: 2,
+                          maximumSize: const Size.fromWidth(200)),
+                      onPressed: navigateToRegisterScreen,
+                      child: const Text('S\'inscrire'),
                     ),
-                  ],
-                ),
-                SizedBox(height: 8),
-                Row(
-                  children: [
-                    SizedBox(
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                            primary: Colors.white,
-                            onPrimary: Colors.black,
-                            elevation: 2,
-                            maximumSize: const Size.fromWidth(200)),
-                        onPressed: navigateToRegisterScreen,
-                        child: const Text('S\'inscrire'),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            )
-          ],
+                  ),
+                ],
+              ),
+            ],
+            // )
+            // ],
+          ),
         ),
       ),
     );
